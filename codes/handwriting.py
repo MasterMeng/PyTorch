3fdbfa888f6f94c7586761eea128672f4e87279d
MasterMeng <mengbin1992@outlook.com>
2019-06-19 17:32:20 +0800

    RNN

--- codes/handwriting.py
+++ codes/handwriting.py
@@ -10,7 +10,7 @@ from torchvision import datasets, transforms
 
 batch_size = 64
 learning_rate = 1e-3
-num_epoches = 5
+num_epoches = 20
 
 # 定义三层全连接神经网络
 
@@ -104,6 +104,22 @@ class CNN(nn.Module):
         x = x.view(x.size(0), -1)
         x = self.fc(x)
         return x
+
+
+class Rnn(nn.Module):
+    def __init__(self, in_dim, hidden_dim, n_layer, n_class):
+        super(Rnn, self).__init__()
+        self.n_layer = n_layer
+        self.hidden_dim = hidden_dim
+        self.lstm = nn.LSTM(in_dim, hidden_dim, n_layer, batch_first=True)
+        self.classifier = nn.Linear(hidden_dim, n_class)
+
+    def forward(self, x):
+        out, _ = self.lstm(x)
+        out = out[:, -1, :]
+        out = self.classifier(out)
+
+        return out
 
 
 data_tf = transforms.Compose(
