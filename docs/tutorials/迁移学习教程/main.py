9dfda8a97fb4323eee06a1778cbcd31e7c6241ad
MasterMeng <mengbin1992@outlook.com>
2019-07-12 20:41:52 +0800

    更新 迁移学习

--- docs/tutorials/迁移学习教程/main.py
+++ docs/tutorials/迁移学习教程/main.py
@@ -56,7 +56,7 @@ def imshow(inp, title=None):
     plt.imshow(inp)
     if title is not None:
         plt.title(title)
-    plt.pause(10)    # 暂停一会，以便更新绘图
+    plt.pause(0.001)    # 暂停一会，以便更新绘图
 
 
 # 获取一批训练数据
@@ -65,4 +65,119 @@ inputs, classes = next(iter(dataloaders['train']))
 # 从批处理中生成网格
 out = torchvision.utils.make_grid(inputs)
 
-imshow(out, title=[class_name[x] for x in classes])
+# imshow(out, title=[class_name[x] for x in classes])
+
+
+def train_model(model, criterion, optimizer, schduler, num_epochs=25):
+    since = time.time()
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs-1))
+        print('-'*10)
+
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                schduler.step()
+                model.train()   # 训练模型
+            else:
+                model.eval()    # 评估模型
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # 零化参数梯度
+                optimizer.zero_grad()
+
+                # 前向传递
+                # 如果只是训练的话，追踪历史
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    # 训练时，反向传播 + 优化
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # 统计
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss / dataset_size[phase]
+            epoch_acc = running_corrects.double() / dataset_size[phase]
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                phase, epoch_loss, epoch_acc))
+
+            # 很拷贝模型
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # 导入最优模型权重
+    model.load_state_dict(best_model_wts)
+    return model
+
+
+def visualize_model(model, num_images=6):
+    was_training = model.training
+    model.eval()
+    images_so_far = 0
+    fig = plt.figure()
+
+    with torch.no_grad():
+        for i, (inputs, labels) in enumerate(dataloaders['val']):
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            outputs = model(inputs)
+            _, preds = torch.max(outputs, 1)
+
+            for j in range(inputs.size()[0]):
+                images_so_far += 1
+                ax = plt.subplot(num_images//2, 2, images_so_far)
+                ax.axis('off')
+                ax.set_title('predicted: {}'.format(class_name[preds[j]]))
+                imshow(inputs.cpu().data[j])
+
+                if images_so_far == num_images:
+                    model.train(mode=was_training)
+                    return
+
+        model.train(mode=was_training)
+
+
+model_ft = models.resnet18(pretrained=True)
+num_ftrs = model_ft.fc.in_features
+model_ft.fc = nn.Linear(num_ftrs, 2)
+
+model_ft = model_ft.to(device)
+
+criterion = nn.CrossEntropyLoss()
+
+# 优化所有参数
+optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+
+# 没7次，学习率衰减0.1
+exp_lr_scheduler = torch.optim.lr_scheduler.StepLR(
+    optimizer_ft, step_size=7, gamma=0.1)
+
+model_ft = train_model(model_ft, criterion, optimizer_ft,
+                       exp_lr_scheduler, num_epochs=25)
+
+visualize_model(model_ft)
